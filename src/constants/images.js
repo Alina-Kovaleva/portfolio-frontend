36f9fd999a4a6c2e534abbb3ef17123490c223e9
Alina Kovaleva <calipco.xp@gmail.com>
2022-08-16 23:11:23 +0200

    Added navigation bar

---
+++ src/constants/images.js
@@ -0,0 +1,33 @@
+import api from '../assets/api.png';
+import css from '../assets/css.png';
+import figma from '../assets/api.png';
+import git from '../assets/git.png';
+import html from '../assets/html.png';
+import javascript from '../assets/javascript.png';
+import node from '../assets/node.png';
+import react from '../assets/react.png';
+import redux from '../assets/redux.png';
+import sass from '../assets/sass.png';
+
+import logo from '../assets/logo.png';
+import bgIMG from '../assets/bgIMG.png';
+import bgWhite from '../assets/bgWhite.png';
+
+import circle from '../assets/circle.svg';
+
+export default {
+  api,
+  css,
+  figma,
+  git,
+  html,
+  javascript,
+  node,
+  react,
+  redux,
+  sass,
+  logo,
+  bgIMG,
+  bgWhite,
+  circle,
+};
