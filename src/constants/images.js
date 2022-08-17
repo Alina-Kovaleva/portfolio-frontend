2b119ccb457f9e017ad5344d5eb99c5ce68dbf0d
Alina Kovaleva <calipco.xp@gmail.com>
2022-08-17 15:37:13 +0200

    Added header section

--- src/constants/images.js
+++ src/constants/images.js
@@ -8,8 +8,14 @@ import node from '../assets/node.png';
 import react from '../assets/react.png';
 import redux from '../assets/redux.png';
 import sass from '../assets/sass.png';
+import python from '../assets/python.png';
+import typescript from '../assets/typescript.png';
 
 import logo from '../assets/logo.png';
+import profile from '../assets/profile.png';
+
+import email from '../assets/email.png';
+
 import bgIMG from '../assets/bgIMG.png';
 import bgWhite from '../assets/bgWhite.png';
 
@@ -26,7 +32,11 @@ export default {
   react,
   redux,
   sass,
+  python,
+  typescript,
   logo,
+  profile,
+  email,
   bgIMG,
   bgWhite,
   circle,
